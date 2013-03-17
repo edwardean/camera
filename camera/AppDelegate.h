9d1f8db1cc704bbcd5de0c4fba835f27e9d33609
stephenkeep <stephen@keep-creative.com>
2013-03-17 14:23:36 +0000

    Initial Commit

---
+++ camera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  camera
+//
+//  Created by Stephen Keep on 17/03/2013.
+//  Copyright (c) 2013 Stephen Keep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
